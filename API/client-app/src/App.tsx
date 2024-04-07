97b9eb2c2a90bd7961415e2f634d260f37858c5e
Surya <surya7ms>
2024-04-07 00:32:33 -0500

    UI app changes

--- API/client-app/src/App.tsx
+++ API/client-app/src/App.tsx
@@ -1,9 +1,29 @@
+import axios from 'axios';
 import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [activites, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+    .then(response => {
+      setActivities(response.data)
+      })
+  },[])
+
   return (
-   <h1>Reactivities</h1>
+    <div>
+     <h1>Reactivities</h1>
+     <ul>
+      {activites.map((activity :any) => (
+        <li key={activity.id}>
+          {activity.title}
+        </li>
+      ))}
+     </ul>
+    </div>
   )
 }
 
